1606a06fa4b0fc97d1decbc89a590044e2f06528
Denis <martushev@skbkontur.ru>
2021-06-28 15:19:51 +0400

    создал заготовку

---
+++ ArrayCounter/ArrayCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
