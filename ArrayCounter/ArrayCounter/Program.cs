d50eab5ca5a21a17af59da901973addac4fa183a
Denis <martushev@skbkontur.ru>
2021-06-28 15:27:18 +0400

    написал метод

--- ArrayCounter/ArrayCounter/Program.cs
+++ ArrayCounter/ArrayCounter/Program.cs
@@ -6,7 +6,20 @@ namespace ArrayCounter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine()?.
+                Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(SumNumbers(numbers));
+        }
+
+        private static string SumNumbers(string[] numbers)
+        {
+            var sum = 0;
+            foreach (var num in numbers)
+            {
+               sum += Convert.ToInt32(num);
+            }
+
+            return sum.ToString();
         }
     }
 }
