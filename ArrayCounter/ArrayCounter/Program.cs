053cb6ba2f96c2165e61d479a8c9b0c5d8e39b17
Denis <martushev@skbkontur.ru>
2021-06-28 16:24:46 +0400

    подправил метод

--- ArrayCounter/ArrayCounter/Program.cs
+++ ArrayCounter/ArrayCounter/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArrayCounter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var numbers = Console.ReadLine()?.
                 Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
             Console.WriteLine(SumNumbers(numbers));
         }
 
-        public static string SumNumbers(string[] numbers)
+        public static int SumNumbers(string[] numbers)
         {
             var sum = 0;
             foreach (var num in numbers)
@@ -19,7 +20,7 @@ namespace ArrayCounter
                sum += Convert.ToInt32(num);
             }
 
-            return sum.ToString();
+            return sum;
         }
     }
 }
